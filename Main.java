04df56a279574f94f2aac6034da889f87063bdef
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2024-04-29 12:45:17 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Negocio;
+
+/**
+ *
+ * @author LabP3E009
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NumeroImaginario objeto = new NumeroImaginario();
+    }
+    
+}
