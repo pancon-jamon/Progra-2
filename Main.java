035d3f6a61faccc3991fef892bc6d7f4e396dd24
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2024-04-30 12:37:18 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
 package Negocio;
 
-/**
- *
- * @author LabP3E009
- */
+import javax.swing.JOptionPane;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        NumeroImaginario objeto = new NumeroImaginario();
+        NumeroComplejo numeroComplejoUno = new NumeroComplejo();//constructor vacio(no por defecto) inmediatamente cuando se define uno el por default ya no existe
+        numeroComplejoUno.setNumeroC();//En este punto ESTE es el objeto actual
+        
+        NumeroComplejo numeroComplejoDos = new NumeroComplejo();//segundo objeto obtenido de la misma clase
+        numeroComplejoDos.setNumeroC();//Ahora ESTE es el objeto actual
+        
+        NumeroComplejo numCompTres;//no necesita ser instanciado porque no va a necesitar los atributos por, ahora solo es un puntero, no un objeto
+        numCompTres = numeroComplejoUno.sumaNumComplejos(numeroComplejoDos);
+        
+        NumeroComplejo numCompRest;//no necesita ser instanciado porque no va a necesitar los atributos por, ahora solo es un puntero, no un objeto
+        numCompRest = numeroComplejoUno.restaNumComplejos(numeroComplejoDos);
+        //String numero = JOptionPane.showInputDialog("Digite 1 si desea ingresar el numero complejo uno o 2 el numero complejo dos"); 
+        JOptionPane.showMessageDialog(null, numeroComplejoUno + "\n" + numeroComplejoDos, "Numeros complejos", 1);//el numero al final me permite desplegar diferentes mensajes predeterminados
+        JOptionPane.showMessageDialog(null,"Su suma es " + numCompTres, "Suma complejos" , 1);//el numero al final me permite desplegar diferentes mensajes predeterminados
+        JOptionPane.showMessageDialog(null,"Su resta es " + numCompRest, "Resta complejos" , 1);
+
     }
     
 }
