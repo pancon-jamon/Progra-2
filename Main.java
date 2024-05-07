cab981913921ca273a58e548bf982a9fae08e8ae
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2024-05-07 12:52:12 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,19 @@
 
 package Fuentes;
 
+import Interfaces.JFEstudiante;
 import java.util.StringTokenizer;
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author LabP3E009
+ * @author Claudia Coello
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Estudiante estudiante = new Estudiante("j", 0, 0, 0, 0);
-        float n1, n2, n3;
-        // TODO code application logic here
-        String linea = JOptionPane.showInputDialog(null, "Ingrese sus notas separadas por una coma", "Notas", 0);
-        StringTokenizer st = new StringTokenizer(linea, ",");
-        
-        n1 = Float.parseFloat(st.nextToken());
-        n2 = Float.parseFloat(st.nextToken());
-        n3 = Float.parseFloat(st.nextToken());
-        estudiante.setEstudiante("J", 0, n1, n2, n3);
-        JOptionPane.showMessageDialog(null, estudiante.toString());
-        JOptionPane.showMessageDialog(null, estudiante.aprueba());
-        //System.out.println(estudiante.aprueba());
+        JFEstudiante jfEstudiante = new JFEstudiante();
+        jfEstudiante.setVisible(true);
     }
     
 }
